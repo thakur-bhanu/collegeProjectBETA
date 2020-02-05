f27644d25979faf25bdb880bf999da41208ff789
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-02-05 23:03:38 +0530

    starting optimizations

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
 def home(request):
-    return HttpResponse("blog home")
+    return render(request, 'blog/home.html')
