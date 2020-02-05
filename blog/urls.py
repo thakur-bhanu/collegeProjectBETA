219a19c71d6d20c6062d9fe7849a6103ffa8f92f
Thakur Bhanu <onlyforworkandstuff@gmail.com>
2020-02-05 22:46:23 +0530

    configuration

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='blog-home'),
+]
